4fbb7b6e54388d2d2c5ac9305040387225e1a0b3
Rawlson Yoshua <gerogeroe12@gmail.com>
2021-06-24 11:07:03 +0700

    fix adapter file directory

--- config/lowdb.js
+++ config/lowdb.js
@@ -1,7 +1,7 @@
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("db.json");
+const adapter = new FileSync("./database/db.json");
 const database = low(adapter);
 
 module.exports = (get, author, message) => {

357496ad97a2b357f162afd778574b41db11e276
Rawlson Yoshua <gerogeroe12@gmail.com>
2021-07-20 09:26:36 +0700

    bug fix lowdb database

--- config/lowdb.js
+++ config/lowdb.js
@@ -4,6 +4,8 @@ const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("./database/db.json");
 const database = low(adapter);
 
+database.defaults({ info: [], history: [] }).write();
+
 module.exports = (get, author, message) => {
   database
     .get(get)
